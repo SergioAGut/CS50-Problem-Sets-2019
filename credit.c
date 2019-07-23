80a6805b81d6dafe372630e712ecfd96e3ae1ce0
SergioAGut <49729141+SergioAGut@users.noreply.github.com>
2019-07-23 18:31:01 -0500

    Final version of credit.c
    Corrected names of card companies, corrected a mistake that let the program call a number INVALID and also display if it fits the more flexible parameters of card issuer.

--- credit.c
+++ credit.c
@@ -20,70 +20,76 @@ void company_check(long long n);
 int main(void)
 {
     long long n = get_long("Number: ");
-    //long long t = n
     int sum = 0;
     
-    for (int j = 2; j >= 1; j -= 1)
+    for (int j = 2; j >= 1; j -= 1) //this loop iterates every two digits. The idea is
+        //to change the starting point with this loop so that I can get odd and even-positioned numbers
     {
         long long t = n;
-        for ( int i = j - 1; t > 0; i += 2)
+        for (int i = j - 1; t > 0; i += 2)
         {
             long long dec = pow(10, i);
             t = n / dec;
             int remainder = t % 10;
             if (j == 2)
+                // here we are doubling every two numbers starting from the second
             {
-                remainder *=2;
+                remainder *= 2;
             }
             if (remainder > 9)
+                // digital sum of products of numbers that have two digits
             {
                 int re_1 = remainder % 10;
                 remainder /= 10;
                 int re_2 = remainder % 10;
                 remainder = re_1 + re_2;
             }            
-            sum += remainder;
+            sum += remainder; //accumulates the sum, which can be done because we already added every digit in the multiplication
         }
         
     }
     if (sum % 10 != 0)
     {
-        printf("INVALID");
+        printf("INVALID\n");
     }
-    else
+    else if (sum % 10 == 0)
     {
-        printf("Great! It is working\n");
+        company_check(n);
     }
-    company_check(n);
+    
+    return 0;
 }
 
 void company_check(long long n)
-    //Calculates length and selects first two digits to know which company issued the card
+//Calculates length and selects first two digits to know which company issued the card
 {
     int length = 0;
     long long t = n;
     //Calculate length by dividing by ten until t is zero
-    for (int i = 0; t > 0; i++ )
+    for (int i = 0; t > 0; i++)
     {
         t /= 10;
         length ++;
     }
-    printf("The length of card is %i\n", length);
     
     // select and check the first two digits of the card number
     int two_digits = n / pow(10, length - 2);
     //checks two_digits with the known types
-    if ((length == 15) &&((two_digits == 34) || (two_digits == 37)))
+    if ((length == 15) && ((two_digits == 34) || (two_digits == 37)))
     {
-        printf("AMERICAN EXPRESS\n");
+        printf("AMEX\n");
     }
-    else if(((length == 13) || (length == 16)) && ((two_digits >= 40) && (two_digits < 50)))
+    else if (((length == 13) || (length == 16)) && ((two_digits >= 40) && (two_digits < 50)))
     {
         printf("VISA\n");
     }
     else if ((length == 16) && ((two_digits >= 51) && (two_digits < 56)))
     {
         printf("MASTERCARD\n");
+    }
+    else
+    {
+        printf("INVALID\n");
     }
                
 }
